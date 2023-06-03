385417256a3261ef1dd02a5d19c752124197f7de
Anchon23 <angellopezrouanet@gmail.com>
2023-06-03 13:08:13 +0200

    rkp

--- main.py
+++ main.py
@@ -28,35 +28,38 @@ print("Número de columnas:", num_columnas)
 # qutar valores NaN usando una función personalizada que use la moda/media para remplazarlos.
 # Si tenemos valores NaN, se reemplazarán por la moda/media de la columna.
 
-def fillna_custom(col):
-    if col.dtype == 'object':
-        fill_value = col.mode()[0]
-    else:
-        fill_value = col.mean()
-    return col.fillna(fill_value)
 try:
+    def fillna_custom(col):
+        if col.dtype == 'object':
+            fill_value = col.mode()[0]
+        else:
+            fill_value = col.mean()
+        return col.fillna(fill_value)
     datos = datos.apply(fillna_custom, axis=0)
 except Exception as e:
     print("Se ha producido un error al reemplazar los valores NaN: ", type(e).__name__)
 
 # Renombrar las columnas para facilitar su manipulación. (Las traducimos de ingles a español)
-datos.rename(columns={'Unnamed': 'id', 'Gender': 'genero', 'EthnicGroup': 'Grupo_etnico_estudiante', 'ParentEduc': 'educacion_padres', 
+datos.rename(columns={'Unnamed: 0': 'id', 'Gender': 'genero', 'EthnicGroup': 'Grupo_etnico_estudiante', 'ParentEduc': 'educacion_padres', 
                       'LunchType': 'tipo_almuerzo', 'TestPrep': 'curso_preparacion', 'ParentMaritalStatus': 'estado_civil_padres', 
                       'PracticeSport': 'frequencia_deporte', 'IsFirstChild': 'esprimerniño', 'NrSiblings':'numeros_hermanos', 
                       'TransportMeans': 'medio_transporte','WklyStudyHours': 'horas_semanales_autoaprendizaje', 'MathScore': 'puntuacion_mates',
                       'ReadingScore': 'puntuacion_lectura', 'WritingScore': 'puntuacion_escrita'}, inplace=True)
 
+# Seleccionar solo columnas numéricas
+columnas_numericas = datos.select_dtypes(include=[int, float]).columns
+
+# Calcular mediana y media de las columnas numéricas
+medianas = datos[columnas_numericas].median()
+medias = datos[columnas_numericas].mean()
+
+# Crear DataFrame con mediana y media
+estadisticas_df = pd.DataFrame({'Columna': columnas_numericas, 'Mediana': medianas, 'Media': medias})
+
+# Imprimir el DataFrame en forma de tabla
+print(estadisticas_df.to_string(index=False))
+
 # 3. Análisis exploratorio de datos
-# Funciones personalizadas para calcular estadísticas
-def calcular_media(columna):
-    return np.mean(columna) 
-
-def calcular_mediana(columna):
-     return np.median(columna) 
-
-def calcular_desv_est(columna):
-    return np.std(columna)
-
 # creamos un bucle para calcular las estadisticas de las columnas numericas
 # si las variables no son numericas nos mostrara un mensaje de error
 try:
@@ -64,9 +67,9 @@ try:
 
     for col in columnas_numericas:
         print(f"Estadísticas para la columna '{col}':")
-        print(f"Media: {calcular_media(datos[col]):.2f}")
-        print(f"Mediana: {calcular_mediana(datos[col]):.2f}")
-        print(f"Desviación estándar: {calcular_desv_est(datos[col]):.2f}")
+        print(f"Media: {np.mean(datos[col]):.2f}")
+        print(f"Mediana: {np.median(datos[col]):.2f}")
+        print(f"Desviación estándar: {np.std(datos[col]):.2f}")
 except Exception as e:
     print("Se ha producido un error al calcular las estadísticas, es posible las variable no sean numericas: ", type(e).__name__)
 
@@ -92,12 +95,15 @@ plt.tight_layout()
 plt.show()
 
 # grafico de barras de la puntuacion de matematicas, lectura y escritura por genero
-pv1=pd.pivot_table(datos,index='genero',values=['puntuacion_mates','puntuacion_lectura','puntuacion_escrita'])
+pv1 = pd.pivot_table(datos, index='genero', values=['puntuacion_mates', 'puntuacion_lectura', 'puntuacion_escrita'])
 plt.style.use('ggplot')
-p1=pv1.plot(kind='barh',y=['puntuacion_mates','puntuacion_lectura','puntuacion_escrita'],edgecolor='black',linewidth=2,figsize=(12,8),title='Hombres vs Mujeres')
-p1.bar_label(p1.containers[0], label_type='edge',padding=0.5,bbox={"boxstyle": "round", "pad": 0.6, "facecolor": "white", "edgecolor": "#1c1c1c", "linewidth" : 2.5, "alpha": 1})
-p1.bar_label(p1.containers[1], label_type='edge',padding=0.5,bbox={"boxstyle": "round", "pad": 0.6, "facecolor": "white", "edgecolor": "#1c1c1c", "linewidth" : 2.5, "alpha": 1})
-p1.bar_label(p1.containers[2], label_type='edge',padding=0.5,bbox={"boxstyle": "round", "pad": 0.6, "facecolor": "white", "edgecolor": "#1c1c1c", "linewidth" : 2.5, "alpha": 1})
+p1 = pv1.plot(kind='barh', y=['puntuacion_mates', 'puntuacion_lectura', 'puntuacion_escrita'], edgecolor='black', linewidth=2, figsize=(12, 8), title='Hombres vs Mujeres')
+
+containers = p1.containers
+
+for container in containers:
+    p1.bar_label(container, label_type='edge', padding=0.5, bbox={"boxstyle": "round", "pad": 0.6, "facecolor": "white", "edgecolor": "#1c1c1c", "linewidth": 2.5, "alpha": 1})
+
 plt.show()
 
 # boxplot de puntuacion total en funcion del nivel de estudios de los padres
@@ -111,21 +117,16 @@ except ValueError as e:
 
 # creamos una nueva columna con el nivel de estudios de los padres pasado a numerico
 df3["formacion_padres"] = df3["educacion_padres"].map({"high school": 1, "some college": 2, "associate's degree": 3, "bachelor's degree": 4, "master's degree": 5})
-
-plt.boxplot([
-    df3[df3["educacion_padres"] == "high school"]["puntuacion_total"],
-    df3[df3["educacion_padres"] == "some college"]["puntuacion_total"],
-    df3[df3["educacion_padres"] == "associate's degree"]["puntuacion_total"],
-    df3[df3["educacion_padres"] == "bachelor's degree"]["puntuacion_total"],
-    df3[df3["educacion_padres"] == "master's degree"]["puntuacion_total"]],
-    labels=["secundaria", "algunos estudios universitarios", "técnico superior", "licenciado", "posgrado"],
-    boxprops=dict(color='black'),
-    medianprops=dict(color='black'), vert=True, patch_artist=True)
+data = []
+labels = ["secundaria", "algunos estudios universitarios", "técnico superior", "licenciado", "posgrado"]
+for i in range(5):
+    data.append(df3[df3["formacion_padres"] == i+1]["puntuacion_total"])
+plt.boxplot(data, labels=labels, boxprops=dict(color='black'), medianprops=dict(color='black'), vert=True, patch_artist=True)
 
 # añadimos la media de cada grupo
 try:
-    for i in range(5):
-        median = round(df3[df3["formacion_padres"] == i+1]["puntuacion_total"].median(), 2)
+    for i, d in enumerate(data):
+        median = round(d.median(), 2)
         plt.text(i+1, median+2, str(median), color='black', ha='center')
 except Exception as e:
     print("Se ha producido un error al añadir las medias: ", type(e).__name__)
@@ -134,6 +135,7 @@ plt.title("Boxplot de puntuacion total en funcion del nivel de estudios de los p
 plt.xlabel("nivel de estudios de los padres")
 plt.ylabel("puntuacion total")
 plt.show()
+
 
 # Gráfico de barras de la puntuación media de cada asignatura en función de la frecuencia de deporte
 fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(12, 4))
@@ -159,13 +161,28 @@ plt.show()
 
 # matriz de correlacion utilizando las variables numericas
 # eliminar una columna de las variables cuantitativas
-datos.drop(['Unnamed: 0'], axis=1, inplace=True)
+datos.drop(['id'], axis=1, inplace=True)
 corr_df = datos.corr()
 print("la correlacion de las variables cuantitativas es:")
 print(corr_df, "\n")
 plt.figure(figsize=(8, 6))
 sns.heatmap(corr_df, annot=True)
 plt.show()
+
+
+# Graph II, III, IV: Correlation with MathScore, ReadingScore, WritingScore
+for score_column in ['puntuacion_mates', 'puntuacion_lectura', 'puntuacion_escrita']:
+    # Create a heatmap of the correlations with the target column
+    corr = data.corr()
+    target_corr = corr[score_column].drop(score_column)
+
+    # Sort correlation values in descending order
+    target_corr_sorted = target_corr.sort_values(ascending=False)
+
+    plt.figure(figsize=(5, 10))
+    sns.heatmap(target_corr_sorted.to_frame(), cmap="coolwarm", annot=True, fmt='.2f')
+    plt.title(f'Correlation with {score_column}')
+    plt.show()
 
 # 4. Manipulación de datos usando condicionales y bucles:
 # filtrar dataset por numero de hermanos mayor a 6 y nivel de estudios de los padres bachelor's degree
@@ -174,7 +191,7 @@ print(datos.loc[(datos['numeros_hermanos'] > 6) & (datos['educacion_padres'] == 
 # filtrar dataset por genero mujer y puntuacion total mayor a 77
 print(datos.loc[(datos['genero'] == "female") & (datos['puntuacion_mates'] > 77),["genero","puntuacion_mates"]])
 
-# mostrar por pantalla los datos del alumno con Unnamed: 0 igual a 13478
+# mostrar por pantalla los datos del alumno con un id igual a 13478
 print(datos.loc[13478])
 
 # filtrar dataset por grupo etnico igual a Group A y puntuacion en lectura ordenada de menor a mayor
